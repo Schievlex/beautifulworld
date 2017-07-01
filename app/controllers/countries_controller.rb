61cb973f7ced460d9ffc027af055d5f03e37e685
lisanne <lisanne.chin@gmail.com>
2017-07-01 15:19:48 +0200

    added edit and delete button

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -7,4 +7,41 @@ class CountriesController < ApplicationController
     @country = Country.find(params[:id])
   end
 
+  def edit
+    @country = Country.find(params[:id])
+  end
+
+  def create
+      @country = Country.new(country_params)
+
+      if @country.save
+         redirect_to @country
+      else
+         render 'new'
+      end
+    end
+
+  def Update
+    @country = Country.find(params[:id])
+
+    if @country.update_attributes(country_params)
+      redirect_to @country
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @country = Country.find(params[:id])
+
+    @country.destroy
+
+    redirect_to countries_path
+  end
+
+  private
+  def country_params
+    params.require(:country).permit(:name, :continent, :image_url)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   root "countries#index"
-  get "countries" => "countries#index"
+  resources :countries
 
-  get "countries/:id" => "countries#show", as: :country
+  #get "countries" => "countries#index"
+
+  #get "countries/new" => "countries#new", as: :new_country
+  #get "countries/:id" => "countries#show", as: :country
+  #post "countries" => "countries#create"
+  #get "countries/:id/edit" => "countries#edit", as: :edit_countries
+
+  #patch "countries/:id" => "countries#update"
+
+  #delete "countries/:id" => "countries#destroy"
+
+
 end
