96ef803a2865bc169431798971167550b5073180
wmwaqas <wmwaqasmahmood@hotmail.com>
2017-07-01 12:48:30 +0200

    Created Place Model

---
+++ app/models/place.rb
@@ -0,0 +1,3 @@
+class Place < ApplicationRecord
+  scope :order_by_name, -> { order(:name) }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+place1 = Place.create!( { name: "Skardu", description: "A Valley in Pakistan", image_url: "http://i4m032imkie3gak4u536h719-wpengine.netdna-ssl.com/wp-content/uploads/2015/06/42.shangrila_-resort-640x426.jpg", rating: 3 } )
+place2 = Place.create!( { name: "K2", description: "A Mountain in Himalayas", image_url: "https://upload.wikimedia.org/wikipedia/commons/8/8c/K2%2C_Mount_Godwin_Austen%2C_Chogori%2C_Savage_Mountain.jpg", rating: 5 } )
+place3 = Place.create!( { name: "Kaghan Valley", description: "A Valley in Pakistan", image_url: "http://4.bp.blogspot.com/-3xhx6dh2Rq8/T88SYAb-RMI/AAAAAAAAAbk/wPjRTAx2DkM/s1600/Kaghan-Valley-Lake-jheel-Saif-ul-Malook.gif", rating: 4 } )
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
