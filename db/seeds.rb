90090e214a2bff3817b7c39e22a6478c653f4156
lisanne <lisanne.chin@gmail.com>
2017-07-01 12:51:41 +0200

    Created model and seed countries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+country1= Country.create! ({name: "Pakistan", image_url: "http://hdfreewallpaper.net/wp-content/uploads/2015/08/eagle-pakistani-flag-wallpapers-free-hd.jpg", continent: "Eurasia"})
+country2= Country.create! ({name: "Ukraine", image_url: "http://proudofukraine.com/wp-content/uploads/2015/06/Ukrainian-flag-meaning-1024x768.jpg", continent: "Eastern Europe" })
