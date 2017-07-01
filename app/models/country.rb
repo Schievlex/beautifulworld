dd0d6cb01ae34def14fe60d2dad7f7f90b2cd26b
lisanne <lisanne.chin@gmail.com>
2017-07-01 15:25:54 +0200

    added validation

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,4 +2,12 @@ class Country < ApplicationRecord
   def self.order_by_name
     order(:name)
   end
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :continent, length: { maximum: 50 }, allow_blank: true
+  validates :image_url, length: { maximum: 255 }, allow_blank: true
+
+
 end
